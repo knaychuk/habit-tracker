c8c101ca3dc1c9ab9c08427ef8a419c5bed4b568
Kate <knaychuk@gmail.com>
2024-10-18 15:11:39 -0700

    add new habit to list

--- popup/habit_tracker.js
+++ popup/habit_tracker.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+  
+  // eventually get from storage
   const habits = [
     "Get Exercise",
     "Drink 8oz Water",
@@ -10,18 +12,35 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('date').textContent = today;
   
   const habitList = document.getElementById('habit-list');
-  habits.forEach(habit=> {
-    const li = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = habit;
-    const label = document.createElement('label');
-    label.htmlFor = habit;
-    label.textContent = habit;
-    li.appendChild(checkbox);
-    li.appendChild(label);
+  const habitForm = document.getElementById('habit-form');
+  const newHabitInput = document.getElementById('new-habit');
 
-    habitList.appendChild(li);
-  });
+  function renderHabits(habits) {
+    habitList.innerHTML = '';
+
+    habits.forEach(habit=> {
+      const li = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.id = habit;
+      const label = document.createElement('label');
+      label.htmlFor = habit;
+      label.textContent = habit;
+      li.appendChild(checkbox);
+      li.appendChild(label);
+  
+      habitList.appendChild(li);
+    });
+  }
+ 
+  habitForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const newHabit = newHabitInput.value.trim();
+    if(newHabit) {
+      habits.push(newHabit);
+      renderHabits(habits);
+    }
+  })
 });
 
