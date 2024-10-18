1a01086b4d59c020e2b225e8ff84ee32b4f005e0
Kate <knaychuk@gmail.com>
2024-10-18 15:58:57 -0700

    remove habit

--- popup/habit_tracker.js
+++ popup/habit_tracker.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   
   // eventually get from storage
-  const habits = [
-    "Get Exercise",
-    "Drink 8oz Water",
-    "Practice LeetCode",
-    "Update Calender",
-  ]
+  // let habits = [
+  //   "Get Exercise",
+  //   "Drink 8oz Water",
+  //   "Practice LeetCode",
+  //   "Update Calender",
+  // ]
+
+  function getStoredHabits() {
+    const storedHabits = localStorage.getItem('habits');
+    return storedHabits ? JSON.parse(storedHabits) : [];
+  }
   
   const today = new Date().toLocaleDateString();
   document.getElementById('date').textContent = today;
@@ -15,10 +20,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const habitForm = document.getElementById('habit-form');
   const newHabitInput = document.getElementById('new-habit');
 
+  
+  const habits = getStoredHabits();
+
   function renderHabits(habits) {
     habitList.innerHTML = '';
 
-    habits.forEach(habit=> {
+    habits.forEach((habit, index)=> {
       const li = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
@@ -26,11 +34,29 @@ document.addEventListener("DOMContentLoaded", () => {
       const label = document.createElement('label');
       label.htmlFor = habit;
       label.textContent = habit;
+      const button = document.createElement('button');
+      button.textContent = 'Remove';
+      button.addEventListener('click', () => {
+        removeHabit(index);
+      })
+
       li.appendChild(checkbox);
       li.appendChild(label);
+      li.appendChild(button);
   
       habitList.appendChild(li);
     });
+  }
+
+
+  function storeHabits(habits) {
+    localStorage.setItem('habits', JSON.stringify(habits));
+  }
+  
+  function removeHabit(index) {
+    habits.splice(index, 1);
+    storeHabits(habits);
+    renderHabits(habits);
   }
  
   habitForm.addEventListener('submit', (e) => {
@@ -39,8 +65,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const newHabit = newHabitInput.value.trim();
     if(newHabit) {
       habits.push(newHabit);
+      storeHabits(habits);
       renderHabits(habits);
     }
   })
+
+  
+  renderHabits(habits);
 });
 
